6ae7ded2dceec4a0d89f6985360a29ea9392979c
Derek Chun <derekchun2013@gmail.com>
2023-12-11 17:04:26 -0500

    Added placeholders

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 inputs = document.querySelectorAll('input');
 inputs.forEach(input => {
-    input.addEventListener('invalid', () => {
-        input.classList.add('submitted');
+    input.addEventListener('invalid', () => { //if input is invalid adds the error class
+        input.classList.add('error');
     });
     input.addEventListener('input',() => {
-        input.classList.remove('submitted');
+        input.classList.remove('error'); //once new input removes error class
     })
 });
+
